873d523a48e35ddaf003fc98aea8f864e5ed9478
Xavi <el_pecas91@hotmail.com>
2024-02-09 11:32:07 +0100

    Inicio de sesion 09/02/24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
 import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { LoginComponent } from './feature/auth/pages/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MenuComponent,MenuSuperiorComponent],
+  imports: [RouterOutlet,MenuSuperiorComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './feature/auth/pages/login/login.component';
+import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { InicioComponent } from './feature/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:InicioComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'menu', component: MenuSuperiorComponent },
+];
